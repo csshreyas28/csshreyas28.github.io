a01a7ae112ec5320c1073628de3bc0da3128c74f
Shreyas C.S. <csshreyas28@gmail.com>
2025-02-06 16:22:40 +0530

    Connect contact route to the server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const contactRoutes = require('./routes/contact');
 
 const app = express();
 app.use(express.json());  // To parse JSON data
@@ -16,6 +17,9 @@ mongoose.connect(process.env.MONGO_URI, {
 })
 .then(() => console.log('MongoDB connected'))
 .catch((err) => console.log(err));
+
+// Use the contact routes
+app.use('/api', contactRoutes);
 
 // Basic route to test server
 app.get('/', (req, res) => {
