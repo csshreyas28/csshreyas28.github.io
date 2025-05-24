acb959b5067d5ff36df47872212e813a15c7865d
Shreyas C.S. <csshreyas28@gmail.com>
2025-05-24 18:00:05 +0530

    bento grids design

--- client-next/src/app/page.tsx
+++ client-next/src/app/page.tsx
@@ -8,21 +8,54 @@ import Projects from "./components/Projects"
 import GitHubStats from "./components/GitHubStats"
 import Contact from "./components/Contact"
 
-
 export default function HomePage() {
   return (
-    <main>
-      <div>
-        <Hello />
-        <GitHub />
-        <LinkedIn />
-        <AboutCard />
-        <Contact />
-        <GitHubStats />
-        <Skills />
-        <Location />
-        <Projects />
-      
+    <main className="min-h-screen bg-gradient-to-br from-gray-100 to-gray-200 dark:from-zinc-900 dark:to-black p-4">
+      <div className="bento-grid">
+
+        {/* Top Greeting */}
+        <div className="col-span-1 lg:col-span-2 glass-card min-h-[150px]">
+          <Hello />
+        </div>
+
+        {/* Socials */}
+        <div className="col-span-1 lg:col-span-1 glass-card">
+          <GitHub />
+        </div>
+        <div className="col-span-1 lg:col-span-1 glass-card">
+          <LinkedIn />
+        </div>
+
+        {/* About (wide card) */}
+        <div className="col-span-1 lg:col-span-2 glass-card">
+          <AboutCard />
+        </div>
+
+        {/* Skills (wide card) */}
+        <div className="col-span-1 lg:col-span-2 glass-card">
+          <Skills />
+        </div>
+
+        {/* GitHub Stats */}
+        <div className="glass-card">
+          <GitHubStats />
+        </div>
+
+        {/* Location */}
+        <div className="glass-card">
+          <Location />
+        </div>
+
+        {/* Contact (wide card) */}
+        <div className="col-span-1 lg:col-span-2 glass-card">
+          <Contact />
+        </div>
+
+        {/* Projects (full width) */}
+        <div className="col-span-1 lg:col-span-4 glass-card min-h-[10px]">
+          <Projects />
+        </div>
+
       </div>
     </main>
   )
